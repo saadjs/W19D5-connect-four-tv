367b99808afefcd9f7a73eb59d52f33630b085da
saad <saad_shaikh@aol.com>
2020-12-18 16:38:30 -0500

    working on phase2 with weird bugs

--- react-app/src/components/GameBoard.js
+++ react-app/src/components/GameBoard.js
@@ -1,71 +1,92 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import {getGame, makeMove} from '../services/gameApi';
-import './GameBoard.css'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getGame, makeMove } from "../services/gameApi";
+import "./GameBoard.css";
 
 function GameBoard(props) {
-  const [grid, setGrid] = useState([[null, null, null, null, null], [null, null, null, null, null], [null, null, null, null, null], [null, null, null, null, null]]);
-  const { gameId } = useParams();
-  // const [column, setColumn] = useState()
+	const [grid, setGrid] = useState([
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+		[null, null, null, null, null],
+	]);
+	const { gameId } = useParams();
+	// const [column, setColumn] = useState()
 
-  useEffect(function () {
-    if (localStorage.getItem(`currentPlayer[${gameId}]`)) {
-      props.setCurrentPlayer(localStorage.getItem(`currentPlayer[${gameId}]`));
-    }
-  }, []);
+	useEffect(function () {
+		if (localStorage.getItem(`currentPlayer[${gameId}]`)) {
+			props.setCurrentPlayer(
+				localStorage.getItem(`currentPlayer[${gameId}]`)
+			);
+		}
+	}, []);
 
-  // update game board
-  useEffect(function () {
-    async function getGameUpdates() {
-      let {game} = await getGame(gameId);
-      setGrid(game.board);
-      props.setPlayer1(game.player1);
-      props.setPlayer2(game.player2);
-    }
-    const intervalHandler = setInterval(() => getGameUpdates(), 1000);
+	// update game board
+	useEffect(
+		function () {
+			async function getGameUpdates() {
+				let { game } = await getGame(gameId);
+				setGrid(game.board);
+				props.setPlayer1(game.player1);
+				props.setPlayer2(game.player2);
+			}
+			const intervalHandler = setInterval(() => getGameUpdates(), 1000);
 
-    return () => clearInterval(intervalHandler);
-  }, [props, gameId]);
+			return () => clearInterval(intervalHandler);
+		},
+		[props, gameId]
+	);
 
-  async function handleClick(e, col_id) {
-    e.preventDefault()
-    e.stopPropagation()
-    const move = await makeMove(gameId, props.currentPlayer, col_id);
-    if (move.success) {
-      props.setCurrentPlayer(props.currentPlayer == props.player1? props.player2:props.player1)
-    }
-    else {
-      console.log(move.error)
-    }
-  }
+	async function handleClick(e, col_id) {
+		e.preventDefault();
+		e.stopPropagation();
+		const move = await makeMove(gameId, props.currentPlayer, col_id);
+		if (move.success) {
+			props.setCurrentPlayer(
+				props.currentPlayer == props.player1
+					? props.player2
+					: props.player1
+			);
+		} else {
+			console.log(move.error);
+		}
+	}
 
-  function buildRow(row_id) {
-    return grid.map((col, col_id) => <div onMouseDown={e => handleClick(e, col_id)} key={`${row_id}${col_id}`} data-row={row_id} data-column={col_id} >{col[row_id]}</div>);
-  }
+	function buildRow(row_id) {
+		return grid.map((col, col_id) => (
+			<div
+				onMouseDown={(e) => handleClick(e, col_id)}
+				key={`${row_id}${col_id}`}
+				data-row={row_id}
+				data-column={col_id}
+			>
+				{col[row_id]}
+			</div>
+		));
+	}
 
+	function otherPlayer() {
+		if (!props.currentPlayer) return "";
+		if (props.player1 === props.currentPlayer) {
+			if (props.player2) {
+				return props.player2;
+			} else {
+				return "Awaiting Opponent";
+			}
+		} else {
+			return props.player1;
+		}
+	}
 
-  function otherPlayer() {
-    if (!props.currentPlayer) return '';
-    if (props.player1 === props.currentPlayer) {
-      if (props.player2) {
-        return props.player2;
-      } else {
-        return "Awaiting Opponent";
-      }
-    } else {
-      return props.player1;
-    }
-  }
-
-  return (
-    <>
-      <h3>Play The Game {gameId}</h3>
-      <b>You: {props.currentPlayer}</b> |<b>Opponent: {otherPlayer()}</b>
-      <div className="game-board">
-        {[0, 1, 2, 3, 4].map((i) => buildRow(i)).reverse()}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h3>Play The Game {gameId}</h3>
+			<b>You: {props.currentPlayer}</b> |<b>Opponent: {otherPlayer()}</b>
+			<div className="game-board">
+				{[0, 1, 2, 3, 4].map((i) => buildRow(i)).reverse()}
+			</div>
+		</>
+	);
 }
 
 export default GameBoard;
--- react-app/src/components/JoinGameForm.js
+++ react-app/src/components/JoinGameForm.js
@@ -1,36 +1,43 @@
-import React, {useState} from "react"
-import  {Redirect} from "react-router-dom"
-import {addPlayer} from "../services/gameApi"
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { addPlayer } from "../services/gameApi";
 
+const JoinGameForm = ({ setPlayer2, setCurrentPlayer }) => {
+	const [gameId, setGameId] = useState("");
+	const [playerName, setPlayerName] = useState("");
+	const [redirect, setRedirect] = useState(null);
 
+	const submit = async (e) => {
+		e.preventDefault();
+		const response = await addPlayer(parseInt(gameId), playerName);
+		console.log(response);
 
-const JoinGameForm = ({setPlayer2}) => {
-    const [gameId, setGameId] = useState("")
-    const [playerName, setPlayerName] = useState("")
-    const [redirect, setRedirect] = useState(null);
+		if (response.game) {
+			setPlayer2(playerName);
+			localStorage.setItem(`player2[${gameId}]`, playerName);
+			setCurrentPlayer(playerName);
+			setRedirect(<Redirect to={`/game/${gameId}`} />);
+		}
+	};
 
-    const submit = async (e) => {
-        e.preventDefault();
-        const response = await addPlayer(parseInt(gameId), playerName);
-        console.log(response);
-
-        if (response.game) {
-            setPlayer2(playerName)
-            localStorage.setItem(`player2[${gameId}]`, playerName)
-            setRedirect(<Redirect to={`/game/${gameId}`}/>)
-        }
-    }
-
-    return (
-        <>
-        {redirect}
-        <form>
-            <input onChange={e=>setGameId(e.target.value)} value={gameId} placeholder="Game ID"></input>
-            <input onChange={e=>setPlayerName(e.target.value)} value={playerName} placeholder="Player Name"></input>
-            <button onClick={submit}>Join Game</button>
-        </form>
-        </>
-    )
-}
+	return (
+		<>
+			{redirect}
+			<form>
+				<input
+					onChange={(e) => setGameId(e.target.value)}
+					value={gameId}
+					placeholder="Game ID"
+				></input>
+				<input
+					onChange={(e) => setPlayerName(e.target.value)}
+					value={playerName}
+					placeholder="Player Name"
+				></input>
+				<button onClick={submit}>Join Game</button>
+			</form>
+		</>
+	);
+};
 
 export default JoinGameForm;
